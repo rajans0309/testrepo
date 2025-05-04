74db847825f2ca3b5598a865dbb9aa812fceaf62
Rajan Singh <127418554+rajans0309@users.noreply.github.com>
2025-05-04 12:46:43 +0530

    Add while loop

--- firstpython.py
+++ firstpython.py
@@ -2,3 +2,12 @@ print("Hello World. This is 1st Python code")
 # loop through 0-10 and print square of number
 for i in range(11):
   print(i**2)
+
+#add while loop
+j=0
+while j<11:
+  print(j*j)
+  j+=1
+
+
+  
